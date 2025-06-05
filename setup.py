d3a8465f5d63b447417a5525086b061d5526dd37
sabagh1994 <sabaghaffari@gmail.com>
2025-06-05 13:47:21 -0500

    moved src to fwpgenmi, updated setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 from setuptools import setup, find_packages
-setup(name='fw-pGENMi', version='0.1', package_dir={'': '.'}, packages=find_packages())
+setup(name='fwpgenmi', version='0.1', package_dir={'': '.'}, packages=find_packages())
